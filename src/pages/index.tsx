dc070c4d25bafaca1685fde8525fc89c08db46de
ETCasual <casualorient@gmail.com>
2022-09-21 19:55:39 +0800

    Remove example images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
-import { FunctionComponent, useEffect, useState } from "react";
+import { FunctionComponent, useEffect } from "react";
 import { trpc } from "../utils/trpc";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { useKeyPress } from "../utils/customHooks";
-
-const exImage = [
-  "https://i.postimg.cc/x8Ww7n1x/IMG-9479.jpg",
-  "https://i.postimg.cc/Vk9xGyQg/IMG-9496.jpg",
-  "https://i.postimg.cc/MHj41Nmy/IMG-9499.jpg",
-  "https://i.postimg.cc/NjwhksVL/IMG-9507.jpg",
-  "https://i.postimg.cc/SsRHxJX1/IMG-9514.jpg",
-  "https://i.postimg.cc/BvMd3xv1/IMG-9517.jpg",
-  "https://i.postimg.cc/1td2nswm/IMG-9522.jpg",
-  "https://i.postimg.cc/yW4GjjL2/IMG-9532.jpg",
-  "https://i.postimg.cc/QMXRv6j3/IMG-9540.jpg",
-  "https://i.postimg.cc/MZCZg8NJ/IMG-9544.jpg",
-  "https://i.postimg.cc/3J2rTcQ4/IMG-9553.jpg",
-];
 
 const Home: NextPage = () => {
   const images = trpc.useQuery(["example.listImages"], {
